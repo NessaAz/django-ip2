5c377b50b299c9c1755ba5421ea897ea4fcc82fb
NessaAz <vanessa.azenwa@gmail.com>
2022-06-22 16:27:44 +0300

    add signup form view function and its urls

--- instaapp/views.py
+++ instaapp/views.py
@@ -1,6 +1,7 @@
 from django.template import loader
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect, get_object_or_404
 from .models import *
+from .forms import *
 
 def index(request):	
 
@@ -9,3 +10,19 @@ def index(request):
 	context = {	}
 
 	return HttpResponse(template.render(context, request))
+
+
+def signup(request):
+    	if request.method == 'POST':
+         form = SignupForm(request.POST)
+         if form.is_valid():
+             username = form.cleaned_data.get('username')
+             email = form.cleaned_data.get('email')
+             password = form.cleaned_data.get('password')
+             User.objects.create_user(username=username, email=email, password=password)
+             return redirect('index')
+         else:
+             form = SignupForm()
+             context = {'form':form,}
+             
+             return render(request, 'signup.html', context)

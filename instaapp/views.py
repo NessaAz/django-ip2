ef0716873bc123c582267bdf9b77c688fe0bc56c
NessaAz <vanessa.azenwa@gmail.com>
2022-06-22 15:42:56 +0300

    link main template to views and urls

--- instaapp/views.py
+++ instaapp/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.template import loader
+from django.shortcuts import render, HttpResponse
 
-# Create your views here.
+def index(request):	
+
+	template = loader.get_template('instaapp/main.html')
+
+	context = {	}
+
+	return HttpResponse(template.render(context, request))

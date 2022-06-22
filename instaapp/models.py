a0802206ad65503c4b290e91ec242213712a099d
NessaAz <vanessa.azenwa@gmail.com>
2022-06-22 20:25:33 +0300

    create Comment and Notification model classes

--- instaapp/models.py
+++ instaapp/models.py
@@ -77,4 +77,47 @@ class Tag(models.Model):
 		return super().save(*args, **kwargs)
 		
 
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments', default='123e4567-f89b-19d3-a656-426614174000')
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    comment = models.CharField(max_length=255, blank=False, null=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
     
+    def user_comment_post(sender, instance, *args, **kwargs):
+        comment = instance
+        post = comment.post
+        text_preview = comment.body[:90]
+        sender = comment.user
+        notify = Notification(post=post, sender=sender, user=post.user, text_preview=text_preview ,notification_type=2)
+        notify.save()
+        
+    def user_del_comment_post(sender, instance, *args, **kwargs):
+        like = instance
+        post = like.post
+        sender = like.user
+        
+        notify = Notification.objects.filter(post=post, sender=sender, notification_type=2)
+        notify.delete()
+        
+        post_save.connect(Comment.user_comment_post, sender=Comment)
+        post_delete.connect(Comment.user_del_comment_post, sender=Comment)
+    
+
+#Comment
+post_save.connect(Comment.user_comment_post, sender=Comment)
+post_delete.connect(Comment.user_del_comment_post, sender=Comment)    
+
+
+#NOTIFICATION
+
+class Notification(models.Model):
+	NOTIFICATION_TYPES = ((1,'Like'),(2,'Comment'), (3,'Follow'))
+
+	post = models.ForeignKey('Post', on_delete=models.CASCADE, related_name="noti_post", blank=True, null=True)
+	sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name="noti_from_user")
+	user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="noti_to_user")
+	notification_type = models.IntegerField(choices=NOTIFICATION_TYPES)
+	text_preview = models.CharField(max_length=90, blank=True)
+	date = models.DateTimeField(auto_now_add=True)
+	is_seen = models.BooleanField(default=False)	

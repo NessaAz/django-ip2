35a94f13bcd1224723ce5a1e74a626d268e41e1f
NessaAz <vanessa.azenwa@gmail.com>
2022-06-22 19:33:46 +0300

    define a newpost view function

--- instaapp/views.py
+++ instaapp/views.py
@@ -4,6 +4,7 @@ from .models import *
 from .forms import *
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
 
 def index(request):	
 
@@ -40,5 +41,47 @@ def loginuser(request):
             return redirect('index') # if you want to redirect same page then, return redirect('this function name in urls.py')
 
     return render(request, 'instaapp/loginuser.html', {'form': form})
+
+
+
+@login_required
+def newpost(request):
+	user = request.user
+	tags_objs = []
+	files_objs = []
+
+	if request.method == 'POST':
+		form = NewPostForm(request.POST, request.FILES)
+		if form.is_valid():
+			files = request.FILES.getlist('content')
+			caption = form.cleaned_data.get('caption')
+			tags_form = form.cleaned_data.get('tags')
+
+			tags_list = list(tags_form.split(','))
+
+			for tag in tags_list:
+				t, created = Tag.objects.get_or_create(title=tag)
+				tags_objs.append(t)
+
+			for file in files:
+				file_instance = PostFileContent(file=file, user=user)
+				file_instance.save()
+				files_objs.append(file_instance)
+
+			p, created = Post.objects.get_or_create(caption=caption, user=user)
+			p.tags.set(tags_objs)
+			p.content.set(files_objs)
+			p.save()
+			return redirect('index')
+	else:
+		form = NewPostForm()
+
+	context = {
+		'form':form,
+	}
+
+	return render(request, 'instaapp/newpost.html', context)
+
+
 
 

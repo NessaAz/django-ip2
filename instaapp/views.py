d2e18681a72943ee3a9de87be65734d09db1810f
NessaAz <vanessa.azenwa@gmail.com>
2022-06-22 22:17:47 +0300

    define userprofile view function

--- instaapp/views.py
+++ instaapp/views.py
@@ -5,6 +5,9 @@ from .forms import *
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
+from django.db import transaction
+from django.urls import resolve
 
 def index(request):	
 
@@ -147,3 +150,56 @@ def like(request, post_id):
         post.save()
         
         return HttpResponseRedirect(reverse('postdetails', args=[post_id]))
+    
+    
+    
+@login_required
+def follow(request, username, option):
+    following = get_object_or_404(User, username=username)
+    try:
+        f, created = Follow.objects.get_or_create(follower=request.user, following=following)
+        
+        if int(option) == 0:
+            f.delete()
+            Stream.objects.filter(following=following, user=request.user).all().delete()
+        else:
+            posts = Post.objects.all().filter(user=following)[:25]
+            with transaction.atomic():
+                for post in posts:
+                    stream = Stream(post=post, user=request.user, date=post.posted, following=following)
+                    stream.save()
+                    
+                    return HttpResponseRedirect(reverse('profile', args=[username]))
+    except:
+        User.DoesNotExist
+                
+    return HttpResponseRedirect(reverse('profile', args=[username]))    
+
+
+
+def UserProfile(request, username):
+    user = get_object_or_404(User, username=username)
+    profile = Profile.objects.get(user=user)
+    url_name = resolve(request.path).url_name
+    
+    if url_name == 'profile':
+        posts = Post.objects.filter(user=user).order_by('-posted')
+        
+    else:
+        posts = profile.favorites.all()
+        posts_count = Post.objects.filter(user=user).count()
+        following_count = Follow.objects.filter(follower=user).count()
+        followers_count = Follow.objects.filter(following=user).count()
+        follow_status = Follow.objects.filter(following=user, follower=request.user).exists()
+        paginator = Paginator(posts, 8)
+        page_number = request.GET.get('page')
+        posts_paginator = paginator.get_page(page_number)
+        
+        template = loader.get_template('profile.html')
+        context = {'posts': posts_paginator,'profile':profile,'following_count':following_count,
+		'followers_count':followers_count,'posts_count':posts_count,'follow_status':follow_status,
+		'url_name':url_name,}
+        
+    return HttpResponse(template.render(context, request))
+
+
